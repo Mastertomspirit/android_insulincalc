d92b448a06ac3df79e47870337e02c6142520577
The Spirit <thespirit@spiritscorp.network>
2026-08-30 21:51:55 +0200

    chore(version) move the update of transitive versions to root build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,23 +79,6 @@ secrets {
   ignoreList.add("FIREBASE_APPCHECK_DEBUG_TOKEN")
 }
 
-configurations.all {
-  resolutionStrategy.eachDependency {
-    if (requested.group == "io.netty") {
-      useVersion("4.1.137.Final")
-    }
-    if (requested.group == "org.bouncycastle") {
-      useVersion("1.84")
-    }
-    if (requested.group == "org.apache.httpcomponents" && requested.name == "httpclient") {
-      useVersion("4.5.14")
-    }
-    if (requested.group == "org.apache.commons" && requested.name == "commons-lang3") {
-      useVersion("3.18.0")
-    }
-  }
-}
-
 dependencies {
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.activity.compose)
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,48 @@ plugins {
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
 }
+
+// Helper funktion
+
+val overrideDependencies = Action<DependencyResolveDetails> {
+  when (requested.group) {
+    "io.netty" -> useVersion("4.1.137.Final")
+    "org.bouncycastle" -> useVersion("1.84")
+    "org.apache.httpcomponents" -> useVersion("4.5.14")
+    "org.apache.commons" -> useVersion("3.18.0")
+  }
+}
+
+// 1. For build plugins
+  buildscript {
+    configurations.all {
+      resolutionStrategy.eachDependency {
+        if (requested.group == "io.netty") {
+          useVersion("4.1.137.Final")
+        }
+        if (requested.group == "org.bouncycastle") {
+          useVersion("1.84")
+        }
+      }
+    }
+  }
+
+// 2. App and test dependencies
+  allprojects {
+    configurations.all {
+      resolutionStrategy.eachDependency {
+        if (requested.group == "io.netty") {
+          useVersion("4.1.137.Final")
+        }
+        if (requested.group == "org.bouncycastle") {
+          useVersion("1.84")
+        }
+        if (requested.group == "org.apache.httpcomponents") {
+          useVersion("4.5.14")
+        }
+        if (requested.group == "org.apache.commons") {
+          useVersion("3.18.0")
+        }
+      }
+    }
+  }
